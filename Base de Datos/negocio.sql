c7d253200c9ff308a4ff599d33c6a8401243edf5
Laura Zamora Beltran <laurita_9505@hotmail.com>
2025-03-17 18:21:32 -0500

    Actualización BBDD

--- Base de Datos/negocio.sql
+++ Base de Datos/negocio.sql
@@ -1,9 +1,9 @@
-/*!999999\- enable the sandbox mode */ 
--- MariaDB dump 10.19  Distrib 10.11.8-MariaDB, for debian-linux-gnu (x86_64)
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19-11.6.2-MariaDB, for Win64 (AMD64)
 --
--- Host: localhost    Database: negocio2
+-- Host: localhost    Database: negocio
 -- ------------------------------------------------------
--- Server version	10.11.8-MariaDB-0ubuntu0.24.04.1
+-- Server version	11.6.2-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,7 +14,7 @@
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
 
 --
 -- Table structure for table `categoria`
@@ -343,10 +343,9 @@ DROP TABLE IF EXISTS `recepcion`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `recepcion` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `fecha` datetime NOT NULL,
   `producto_id` int(11) NOT NULL,
-  `cantidad` bigint(20) NOT NULL,
-  `peso` varchar(255) NOT NULL,
+  `cantidad` varchar(225) DEFAULT NULL,
+  `peso` int(11) DEFAULT NULL,
   `devolucion` varchar(255) NOT NULL,
   `proveedor_id` int(11) NOT NULL,
   `inventario_id` int(11) NOT NULL,
@@ -536,10 +535,6 @@ LOCK TABLES `usuario_rol` WRITE;
 /*!40000 ALTER TABLE `usuario_rol` DISABLE KEYS */;
 /*!40000 ALTER TABLE `usuario_rol` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping routines for database 'negocio2'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -548,6 +543,6 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
 
--- Dump completed on 2025-03-10 18:37:21
+-- Dump completed on 2025-03-15 14:28:01
